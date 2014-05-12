29b693f415bd7d9924d223315c4db6c7d3b026d8
bcowzer <bcowzer@hotmail.com>
2014-05-12 13:23:21 +0100

    update app

--- www/toDoController.js
+++ www/toDoController.js
@@ -1,12 +1,30 @@
- function TodoCtrl($scope) {
-    $scope.todos = [
-    {task:'do something', detail:'do a thing'},
-    {task:'do something else', detail:'do another thing'}];
-    
-    $scope.addNewTask = function() {
-        $scope.todos.push ({task:$scope.newTask});
-        $scope.newTask = '';
-    };
-        
-    
-}
+	function ToDoController($scope) {
+
+		$scope.toDoList=[
+            {task:'some task', detail:'some details of task'}, 
+            {task:'some other task', detail:'some other details'}
+		];
+
+
+		$scope.addNewTask = function () {
+			$scope.toDoList.push(
+
+				{
+					task: $scope.newTask.task,
+					detail: $scope.newTask.detail
+				}
+			);
+            $scope.newTask.task= '';
+            $scope.newTask.detail='';
+		}
+
+
+$scope.delete = function ( index ) {
+
+    $scope.toDoList.splice(index, 1);
+
+};
+
+	}
+
+	console.log('got it...........');
